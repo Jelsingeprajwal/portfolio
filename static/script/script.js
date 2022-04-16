48cee92c28df0061cacdb35032731bb4b6e3c7cd
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2022-04-16 17:30:04 +0530

    heroku setup

--- static/script/script.js
+++ static/script/script.js
@@ -1,17 +1,16 @@
-console.log("Hello world");
 const menuBtn = document.querySelector('#menu-bar');
 const navContainer = document.querySelector('.nav-container');
 const darkBtn = document.querySelector('#dark-toggle img');
-const contactForm=document.querySelector('#contact-form');
+const contactForm = document.querySelector('#contact-form');
 const name = document.querySelector('#form-name');
-const email= document.querySelector('#email');
-const message= document.querySelector('#msg')
+const email = document.querySelector('#email');
+const message = document.querySelector('#msg')
+
+
 //Navbar mobile view toggle
 
-menuBtn.addEventListener('click',(e)=>{
-navContainer.classList.toggle('on');
+menuBtn.addEventListener('click', (e) => {
+    navContainer.classList.toggle('on');
 });
 
-document.querySelector('.about').addEventListener('scroll',()=>{
-    console.log('scrolled');
-});
+
