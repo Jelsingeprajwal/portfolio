7080bfca32d22fe7193a91c0ece0fef4b9c7ee62
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2023-08-28 15:28:52 +0530

    setup basic boilerplate

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryBlue: "#184AFF",
+        secondaryBlue: "rgba(22, 30, 61, 0.98)",
+        secondaryText: "#7C7474",
+        dimWhite: "#D9D9D9",
+        dimBlue: "rgba(24, 75, 255, 0.06)",
+      },
+      fontFamily: {
+        GeneralSans: ["General Sans"],
+        Hind: ["Hind"],
+      },
+    },
   },
   plugins: [],
 };
